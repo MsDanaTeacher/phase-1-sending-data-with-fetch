580fbcb47e4212b51a5ee19bdc6b72690c249261
Dana Lobell <danalobell@gmail.com>
2022-08-06 13:26:14 -0400

    commit tree d7dc5436486fbac56d633c0ffeaf43b9e44351e6

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your code here
+function submitData(name, email) {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json',
+        },
+        body: JSON.stringify({
+            name: "Sam",
+            email: "sam@sam.com",
+        })
+        .then(res => res.json())
+        .then(users => console.log(users))
+    })
+}
